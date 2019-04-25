169680f6222db05b8c0946ef4529a0e8da12473f
experience <14246657+experiencedft@users.noreply.github.com>
2019-04-25 23:22:35 +0100

    Add Firebase Authentication
    Users now have to sign up / log in to be able to use the extension. The MVP uses Firebase for quick prototyping.

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,55 @@
-//Fired on install or on update (extension or browser)
-chrome.runtime.onInstalled.addListener(function() {
+var config = {
+    apiKey: "AIzaSyBiWzrlcwxigAnXrapXNX2pIp3yj5WFTG4",
+    authDomain: "defacto-654d7.firebaseapp.com",
+    databaseURL: "https://defacto-654d7.firebaseio.com",
+    projectId: "defacto-654d7",
+    storageBucket: "defacto-654d7.appspot.com",
+    messagingSenderId: "633832248132"
+  };
+  firebase.initializeApp(config);
+
+//Global listener for all popup messages
+chrome.runtime.onMessage.addListener(
+    function(message, sender, sendResponse) {
   
-})
+      if (message.type == "loginAttempt") {
+
+        //If the user logs in using the login button
+        /* console.log(sender.tab ?
+                    "from a content script:" + sender.tab.url :
+                    "from the extension"); */
+        firebase.auth().signInWithEmailAndPassword(message.email, message.password).catch(function(error) {
+            //TODO: add error message if wrong email/password
+        });
+  
+      } else if (message.type == "signUpAttempt") {
+
+        firebase.auth().createUserWithEmailAndPassword(message.email, message.password).catch(function(error) {
+            //TODO: add error if there is a sign-up issue (email already used for example...)
+        });
+
+      } else if (message.type == "popupLoad"){
+  
+        //Get current authentication state and tell popup
+        let user = firebase.auth().currentUser;
+        if (user) {
+          sendResponse({type: "loggedIn"});
+        } else {
+          sendResponse({type: "loggedOut"});
+        };
+
+      };
+  
+    });
+  
+  
+
+  //Tells the popup script when the auth state changes
+  //TODO: Investigate what happens if a profile is deleted from the console
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      chrome.runtime.sendMessage({loginStatus: "loggedIn", type: "authChange"});
+    } else {
+      chrome.runtime.sendMessage({loginStatus: "loggedOut", type: "authChange"});
+    }
+  });
---
+++ extension/popup.js
@@ -0,0 +1,204 @@
+//Helper functions to build the HTML 
+
+function makeSignInMenu() {
+  let HTMLstring = "<button id='sign-up'>Sign-up</button>\
+  <button id='log-in'>Log-in</button>";
+  return HTMLstring;
+};
+
+function makeLogInForm() {
+  let HTMLstring = "\
+    <div> \
+      <label>Email</label>\
+      <input id='input-email'>\
+    </div> \
+    <div>\
+      <label>Password</label>\
+      <input id='input-password'>\
+    </div> \
+    <button id='log-in'>Sign-in</button>"
+  return HTMLstring;
+};
+
+function makeSignUpForm() {
+  let HTMLstring = "\
+    <div> \
+      <label>Email</label>\
+      <input id='input-email'>\
+    </div> \
+    <div>\
+      <label>Password</label>\
+      <input id='input-password'>\
+    </div> \
+    <div>\
+    <label>Confirm password</label>\
+    <input id='input-confirm-password'>\
+    </div> \
+    <button id='sign-up'>Sign-up</button>"
+    return HTMLstring;
+};
+
+function makeDeFactoMenu() {
+  let HTMLstring = "<button type='button' name='submission' id='submission'>Submit current tab</button>\
+  <button type='button' name='review' id='review'>Start reviewing</button>";
+  return HTMLstring;
+};
+
+
+//To do when the user clciks on the exntension icon 
+window.addEventListener('DOMContentLoaded', (_event) => {
+  console.log("DOM loaded and parsed");
+  //Send a message to background script informing that the page is loaded
+  chrome.runtime.sendMessage({type: "popupLoad"}, function (response) {
+    //Background script tells us whether the user is logged in
+    if (response.type == "loggedIn") {
+
+      //Create HTML of DeFacto UI
+      let container = document.querySelector("#container");
+      container.innerHTML = makeDeFactoMenu();
+      //Add event listeners
+      let submission = document.getElementById('submission');
+      let review = document.getElementById('review');
+      submission.onclick = function() {
+        //Get all tabs with specified property (active tab of the active window)
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+          var activeTab = tabs[0].url
+          //TODO: plug to the API, currently uses local storage for testing
+          chrome.storage.sync.get(['queue'], function(result) {
+            //If queue is not in sync storage, create it
+            if (result.queue === undefined) {
+              result.queue = [];
+            };
+            //Add active tab URL if not already there
+            if (!(result.queue.includes(activeTab))) {
+            result.queue.push(activeTab);
+            chrome.storage.sync.set(result, function() {
+              //test
+              console.log(result.queue)
+            });
+            };
+          });
+        });
+      //  chrome.storage.sync.get(['queue'], function(result) {
+      //    console.log(result.queue);
+      //  });
+      };
+      review.onclick = function() {
+        chrome.tabs.create({url: "reviewTab.html"});
+      };
+
+    } else if (response.type == "loggedOut") {
+
+      let container = document.querySelector("#container");
+      container.innerHTML = makeSignInMenu();
+      //Add on click event listener to log-in button
+      let logInButton = document.querySelector("#log-in");
+      logInButton.onclick = function () { 
+        container.innerHTML = makeLogInForm();
+        //Add event listener to log-in form button
+        let logIn = document.querySelector("#log-in");
+        logIn.onclick = function () {
+          let credentials = {};
+          credentials.email = document.querySelector("#input-email").value;
+          credentials.password = document.querySelector("#input-password").value;
+          credentials.type = "loginAttempt";
+          container.innerHTML = "loading...";
+          //Send crendentials to background script;
+          chrome.runtime.sendMessage(credentials)
+
+        };
+      };
+      //Add on click even listener to the sign-up button
+      let signUpButton = document.querySelector("#sign-up");
+      signUpButton.onclick = function () {
+        container.innerHTML = makeSignUpForm();  
+        //Add event listener to  sign-up form button
+        let signUp = document.querySelector("#sign-up");
+        signUp.onclick = function () {
+          if (document.querySelector("#input-password").value == document.querySelector("#input-confirm-password").value) {
+            let credentials = {};
+            credentials.email = document.querySelector("#input-email").value;
+            credentials.password = document.querySelector("#input-password").value;
+            credentials.type = "signUpAttempt";
+            container.innerHTML = "loading...";
+            //Send crendentials to background script;
+            chrome.runtime.sendMessage(credentials)
+          } else {
+            //TO DO: Tell user that the passwords don't match
+          }
+        };
+      };
+    };
+  });
+});
+
+//Listen to messages from the background script
+chrome.runtime.onMessage.addListener( function(message, _sender, _sendResponse) {
+  //If the message signals an auth state change
+  if (message.type == "authChange") {
+
+    if (message.loginStatus == "loggedIn") {
+
+      //Create HTML of DeFacto UI
+      let container = document.querySelector("#container");
+      container.innerHTML = makeDeFactoMenu();
+      //Add event listeners
+      let submission = document.getElementById('submission');
+      let review = document.getElementById('review');
+      submission.onclick = function() {
+        //Get all tabs with specified property (active tab of the active window)
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+          var activeTab = tabs[0].url
+          //TODO: plug to the API, currently uses local storage for testing
+          chrome.storage.sync.get(['queue'], function(result) {
+            //If queue is not in sync storage, create it
+            if (result.queue === undefined) {
+              result.queue = [];
+            };
+            //Add active tab URL if not already there
+            if (!(result.queue.includes(activeTab))) {
+            result.queue.push(activeTab);
+            chrome.storage.sync.set(result, function() {
+              //test
+              console.log(result.queue)
+            });
+            };
+          });
+        });
+      //  chrome.storage.sync.get(['queue'], function(result) {
+      //    console.log(result.queue);
+      //  });
+      };
+      review.onclick = function() {
+        chrome.tabs.create({url: "reviewTab.html"});
+      };
+
+    } else if (message.loginStatus == "loggedOut") {
+
+      //If user logs out, re-build the sign-in page
+      let container = document.querySelector("#container");
+      container.innerHTML = makeSignInMenu();
+      //Add on click event listener to log-in button
+      let logInButton = document.querySelector("#log-in");
+      logInButton.onclick = function () { 
+        container.innerHTML = makeLogInForm();
+        //Add event listener to 
+        let logIn = document.querySelector("#log-in");
+        logIn.onclick = function () {
+          let credentials = {};
+          credentials.email = document.querySelector("#input-email").value;
+          credentials.password = document.querySelector("#input-password").value;
+          credentials.type = "loginAttempt";
+          container.innerHTML = "loading...";
+          //Send crendentials to background script;
+          chrome.runtime.sendMessage(credentials)
+
+        };
+      }
+    };
+
+  } else if (message.type == "dummy") {
+
+  };
+});
+
