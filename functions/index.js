9f6d8f064600c7fcaec677b2df8fa4ec203bfa47
experience <14246657+experiencedft@users.noreply.github.com>
2019-05-01 23:48:00 +0100

    Add Timestamp to Pushed URLs

--- functions/index.js
+++ functions/index.js
@@ -2,3 +2,11 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+//When a new item is pushed to queue, add the timestamp
+exports.addTimestampUrl = functions.database.ref('/queue/{pushId}')
+    .onCreate( (snapshot, context) => {
+        const id = context.params.pushId;
+        let newVal = snapshot.val();
+        newVal.timestamp = admin.database.ServerValue.TIMESTAMP;
+        return snapshot.ref.set(newVal);
+    });
