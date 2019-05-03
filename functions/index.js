461da91e56247ea2fd3b44ca66e4af0016c7e33c
experience <14246657+experiencedft@users.noreply.github.com>
2019-05-03 23:56:33 +0100

    Add New Users To Database

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,18 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+
+
+//When a user signs up, add the UID to the database
+exports.addAccount = functions.auth.user().onCreate((user) => {
+    const id = user.uid;
+    const email = user.email;
+    console.log(id);
+    return admin.database().ref("/users/"+id).set({"email": email, "uid": id}); 
+});
+
 //When a new item is pushed to queue, add the timestamp
+//Also distribute it to a subset of users that are notified
 exports.addTimestampUrl = functions.database.ref('/queue/{pushId}')
     .onCreate( (snapshot, context) => {
         const id = context.params.pushId;
